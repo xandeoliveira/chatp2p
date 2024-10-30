5adb644de2cf03d59867b8c23dc9dc60c112bb67
Alexandre Oliveira <oliveiraalexandrelima@hotmail.com>
2024-10-30 14:47:08 -0300

    Inclusão do chat ainda descriptografado.

--- main.go
+++ main.go
@@ -1,29 +1,92 @@
 package main
 
-// func receive(w webview.WebView, message string) {
-// 	w.Eval(fmt.Sprintf("postUserMessage('%s')", message))
-// }
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"path/filepath"
 
-// func send(message string) string {
-// 	return message
-// }
+	webview "github.com/webview/webview_go"
+)
+
+const porta = ":9000"
 
 func main() {
-	// INTERFACE GRÁFICA
-	// path, err := filepath.Abs("layout/index.html")
-	// if err != nil {
-	// 	os.Exit(1)
-	// }
-	// path = "file://" + path
+	path, err := filepath.Abs("layout/index.html")
+	if err != nil {
+		os.Exit(1)
+	}
+	path = "file://" + path
 
-	// w := webview.New(true)
-	// defer w.Destroy()
+	w := webview.New(true)
+	defer w.Destroy()
 
-	// w.SetTitle("Chat Unilab")
-	// w.SetSize(800, 500, 0)
-	// w.Navigate(path)
+	w.SetTitle("Chat Unilab")
+	w.SetSize(800, 500, 0)
+	w.Navigate(path)
 
-	// w.Bind("send", send)
+	w.Bind("getIp", func(ip string) {
+		go client(w, ip)
+	})
 
-	// w.Run()
+	server(w)
+
+	w.Run()
+}
+
+func server(w webview.WebView) {
+	server, err := net.Listen("tcp", porta)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := server.Accept()
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		go read(w, conn)
+	}
+}
+
+func read(w webview.WebView, conn net.Conn) {
+	for {
+		var buffer = make([]byte, 256)
+		nbytes, err := conn.Read(buffer)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if nbytes > 0 {
+			// Nesta parte devo colocar a mensagem com js
+			// fmt.Println(conn.RemoteAddr().String() + ">=< : " + string(buffer))
+			w.Eval(fmt.Sprintf("postUserMessage('%s')", string(buffer)))
+		}
+
+	}
+}
+
+func client(w webview.WebView, ip string) {
+	conn, err := net.Dial("tcp", ip+porta)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.Bind("send", func(msg string) {
+		_, err = conn.Write([]byte(msg))
+
+		if err != nil {
+			log.Fatal(err)
+		}
+	})
+
 }
